34ac7d87ae3bc05aef4ef09e5a2b91628dc665fe
mpab <mick@Mage-D.abs.com>
2017-09-17 18:58:52 +0200

    tweaked interface

--- my_agents/control_cartpole_random_search.py
+++ my_agents/control_cartpole_random_search.py
@@ -2,15 +2,8 @@ import gym
 import random
 import numpy as np
 
-from statistics import median, mean
-from collections import Counter
-
-LR = 1e-3
 env = gym.make("CartPole-v0")
 env.reset()
-goal_steps = 500
-score_requirement = 50
-initial_games = 10000
 
 def random_search_control_run_episode(required_reward, env, parameters):
     
@@ -21,7 +14,8 @@ def random_search_control_run_episode(required_reward, env, parameters):
 
     for _ in range(required_reward):
         
-        env.render()
+        #env.render()
+        env.render(close=True)
 
         action = 0 if np.matmul(parameters, observation) < 0 else 1
         observation, reward, done, info = env.step(action)
@@ -32,7 +26,7 @@ def random_search_control_run_episode(required_reward, env, parameters):
 
     return totalreward
 
-def random_search_control():
+def random_search_control(start_parameters = None):
 
     required_reward = 5000
 
@@ -40,7 +34,10 @@ def random_search_control():
     bestreward = 0
 
     for _ in range(1000):
-        parameters = np.random.rand(4) * 2 - 1
+        if (start_parameters == None):
+            parameters = np.random.rand(4) * 2 - 1
+        else:
+            parameters = start_parameters
         
         reward = random_search_control_run_episode(required_reward, env, parameters)
 
@@ -53,5 +50,7 @@ def random_search_control():
         if (reward == required_reward):
             print ("Success!")
             break
-            
-random_search_control()
+
+if __name__ == "__main__":
+    random_search_control([ 0.26594176, 0.31772193, 0.92617377, 0.35116463])
+    #random_search_control()
